17c33ed22d52e070cc4f3ec84b4816a07fcec59f
John Doe <thepeoplescoder@gmail.com>
2024-01-22 14:27:03 -0800

    Factored out MILLIS_PER_SECOND in the return value for toMilliseconds().

--- src/modules/toMilliseconds.js
+++ src/modules/toMilliseconds.js
@@ -2,9 +2,9 @@ import { MILLIS_PER_SECOND, SECONDS_PER_MINUTE, MINUTES_PER_HOUR } from "./const
 
 function toMilliseconds({ hours, minutes, seconds, milliseconds }) {
     return (milliseconds || 0) +
-           (seconds      || 0) * MILLIS_PER_SECOND +
-           (minutes      || 0) * SECONDS_PER_MINUTE * MILLIS_PER_SECOND +
-           (hours        || 0) * MINUTES_PER_HOUR * SECONDS_PER_MINUTE * MILLIS_PER_SECOND;
+           MILLIS_PER_SECOND * ((seconds || 0) +
+                                (minutes || 0) * SECONDS_PER_MINUTE +
+                                (hours   || 0) * MINUTES_PER_HOUR * SECONDS_PER_MINUTE);
 };
 
 export default toMilliseconds;
